6577883751055c4812524e2ecb26d0f1275d91c8
Y Abe <1718005.ay@sist.ac.jp>
2020-07-22 22:18:38 +0900

    submit->click,enter

--- ggttmm.js
+++ ggttmm.js
@@ -1,21 +1,16 @@
-
+const APPTAG="[ggttmm]:";
 var myLocalStrage;
 
 $(function(){
-    var pageUrl=location.href;
-    // if(!pageUrl.match("google.com"))return;
-    
-    console.log("Hello");
-
     myLocalStrage=localStorage;
     $("div.rc").each(function(){
         var elem=this;
         var url = $(this).find("a").attr("href");        
-        var val=(localStorage.getItem("ggttmm:"+url))?localStorage.getItem("ggttmm:"+url).replace("ggttmm:",""):"";  
+        var val=(localStorage.getItem(APPTAG+url))?localStorage.getItem(APPTAG+url).replace(APPTAG,""):"";  
         var formHtml=
             "<form class='myForm' "+((val=="")?"style='display:none'":"style='display:block'")+"' >"+
                 "<input type='text' class='myText' placefolder='' value='"+val+"' />"+
-                "<input type='button' class='myButton' value='保存' />"+
+                "<input type='button' class='myButton' value='保存'  />"+
                 "<input type='text' class='myHiddenText' style='display:none' />"+
                 "<input type='hidden' class='myHidden' value='"+url+"' />"+
             "</form>";
@@ -25,38 +20,40 @@ $(function(){
     // $(".myForm").submit(function(e){
     //     return e.preventDefault(); 
     // });    
-});
 
-$(".myText").keydown=function(e){
-    console.log("Press:"+e);
-    var key=e.keyCode || e.which;
-    if(key==13){
-        changeStorage(e);
-        document.activeElement.blur();
-    }
-}
-$(".myButton").on("click",function(e){
-    changeStorage(e.parent());
+    $(".myText").on("keypress",function(e){
+        var key=e.keyCode || e.which;
+        if(key==13){
+            changeStorage($(this).parent());
+            document.activeElement.blur();
+        }
+    });
+    $(".myButton").on("click",function(e){
+        changeStorage($(this).parent());
+    });
 });
 
 function changeStorage(element){
-    console.log("Chaneg Status!!!");
     var url=$(element).find(".myHidden").val();
     var text=$(element).find(".myText").val();
     if(text==null || text==""){
-        localStorage.removeItem("ggttmm:"+url);
+        localStorage.removeItem(APPTAG+url);
     }else{
-        localStorage.setItem("ggttmm:"+url,text);
+        localStorage.setItem(APPTAG+url,text);
     }
+
 }
 
 chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse) {    
-      console.log(request.greeting);
+    //   console.log(request.greeting);
       switch(request.greeting){
-        case "changeHiddenStatus":changeHiddenStatus();
+        case "changeHiddenStatus":
+            changeHiddenStatus();
             break;
-        case "getLocalStorage":sendResponse({farewell:localStorage});
+        case "getLocalStorage":
+            sendResponse({farewell:"Hello"});
+            console.log("send");
             break;
         default:break;
       }
